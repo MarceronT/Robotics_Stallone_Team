34a80ba2b99aeace2aa016ee812df3992e6136d4
Thomas Marceron <thomas.marceron@greensocs.com>
2022-03-24 17:28:45 +0100

    Version 2 follow me function
    - Works in real
    
    Signed-off-by: Thomas Marceron <thomas.marceron@greensocs.com>

--- laser_scan.py
+++ laser_scan.py
@@ -1,5 +1,6 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.qos import qos_profile_sensor_data
 
 from std_msgs.msg import String
 from sensor_msgs.msg import LaserScan
@@ -11,20 +12,20 @@ class MinimalSubscriber(Node):
 
     def __init__(self):
         super().__init__('minimal_subscriber')
-        self.dist_max = 2.5
-        self.dist_min = 0.5
+        self.dist_max = 0.60 # 1.0
+        self.dist_min = 0.20 # 0.20
         # self.sum_x = 0
         # self.sum_y = 0
-        self.x_centre = (self.dist_max - self.dist_min) / 2
+        self.x_centre = (self.dist_max + self.dist_min) / 2
         self.y_centre = 0
         self.v_lin = 0
         self.v_rot = 0
-        self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.publisher = self.create_publisher(Twist, '/cmd_vel', 10)
         self.subscription = self.create_subscription(
             LaserScan,
             'scan',
             self.listener_callback,
-            10)
+            qos_profile_sensor_data)
         self.subscription  # prevent unused variable warning
 
     def listener_callback(self, msg): 
@@ -39,7 +40,7 @@ class MinimalSubscriber(Node):
             i += 1
             theta = msg.angle_min +msg.angle_increment * i
             theta_degrees = math.degrees(theta)
-            if (300 < theta_degrees or theta_degrees < 60) :
+            if (330 < theta_degrees or theta_degrees < 30) :
                 if (r > self.dist_min and r < self.dist_max):
                     print('Theta ={}, r ={}'.format(theta_degrees,r))
                     x = r * math.cos(theta)
@@ -59,10 +60,12 @@ class MinimalSubscriber(Node):
             delta_x = bari_x - self.x_centre
             delta_y = bari_y
 
-            self.v_lin = delta_x * 1.3
-            self.v_rot = delta_y * 2
+            self.v_lin = delta_x * 0.5
+            print(self.v_lin)
+            self.v_rot = delta_y * 1.8
+            print(self.v_rot)
 
-        msg_twist.linear.x = self.v_lin
+        msg_twist.linear.x = self.v_lin # should be between 0.01 and 0.1
         msg_twist.angular.z = self.v_rot
         self.publisher.publish(msg_twist)
 
